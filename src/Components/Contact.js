b4985d9db3c30f072440675029ed0121f13739cb
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-03 18:15:12 -0700

    added two links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,12 +7,21 @@ render(){
         <div>
             <p>
                 <h5>Want to work together? Get in touch!</h5>
-                Phone: <Mailto tel='720-749-0720'/><br/>
+                Phone:<Mailto tel='720-749-0720'/><br/>
                 Email: <Mailto
                     email='dlunoe@yahoo.com'
                     headers={
                         {subject: 'Question from your website'}
-                    }/>
+                    }/><br/>
+                GitHub: <a
+                 href="https://github.com/Dlunoe"
+                 target="_blank">https://github.com/Dlunoe</a>
+                 <br/>
+                 LinkedIn:<a
+                 href="https://www.linkedin.com/in/dustinlunoe/"
+                 target="_blank">
+                    https://www.linkedin.com/in/dustinlunoe/
+                 </a>
             </p>
         </div>
     )
