b9d7819bd278f8766524ef850e103c2022aef9a0
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-05 14:50:27 -0700

    added react-mdl

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header'
 import Main from './Components/Main'
 
+
 import {Route, Switch, Redirect} from 'react-router-dom';
 
 function App() {
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
-import '../App.css'
+import {ProgressBar} from 'react-mdl';
+
+import '../App.css';
+import BackSkills from './BackSkills'
 
 class Home extends Component{
 render(){
     return(
         <div>
-            <h1>Dustin Lunoe</h1>
-            <h3 class="brand">Software developer who uses his experience and skills in managing people and customer experience.
+            <div>
+                <h1>Dustin Lunoe</h1>
+                <h3 class="brand">Software developer who uses his experience and skills in managing people and customer experience.
                  As a developer I have a knack for problem solving,
                  while also maintaining a perspective of the end-user experience</h3>
+            </div>
+            <div>
+                <ProgressBar progress={50} />
+            </div>
         </div>
+        
     )
 }
 }
