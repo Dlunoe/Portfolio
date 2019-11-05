82a3b64df291a5d76957220320c16001447ccee1
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-04 22:02:04 -0700

    styled buttons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,15 +4,15 @@ import '../App.css'
 function Projects(){
     return(
         <div className="projects">
-            <p>this is Projects</p>
+            <p class="projectHead">this is Projects</p>
             <ul class="projectList">
-                <li>
+                <li class="diaries">
                     <a target="_blank" href="https://safe-citadel-26643.herokuapp.com/" 
                     class="diaries">
                         <button class="diariesButton">Diaries</button>
                     </a>
                 </li>
-                <li>
+                <li class="reviews">
                     <a target="_blank" href="https://reviews-app-client.herokuapp.com/" 
                     class="reviews">
                         <button class="reviewsButton">Reviews</button>
