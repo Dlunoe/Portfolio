bf0050d4aefea6bf4414540f1683f901282fbf4b
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-07 13:19:30 -0700

    added github links

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,17 +6,19 @@ function Projects(){
     return(
         <div className="projects">
             <ul class="projectList">
-                <li class="diaries">
+                <li class="listItem">
                     <a target="_blank" href="https://safe-citadel-26643.herokuapp.com/" 
                     class="diaries">
                         <button class="diariesButton">Diaries</button>
                     </a>
+                    <a class="gitLink" href="https://github.com/Dlunoe/Project-Reviews">GitHub</a>
                 </li>
-                <li class="reviews">
+                <li class="listItem">
                     <a target="_blank" href="https://reviews-app-client.herokuapp.com/" 
                     class="reviews">
                         <button class="reviewsButton">Reviews</button>
                     </a>
+                    <a class="gitLink" href="https://github.com/Dlunoe/ProjectGamereviews">GitHub</a>
                 </li>
             </ul>
         </div>
