1592ff187e5c6e43365bffd97001ec9c32f19a74
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-09 16:04:38 -0700

    styled and added wakeup code

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../Components/Styles/projects.css'
 import '../App.css'
 
-function Projects(){
-    return(
-        <div className="projects">
-            <ul class="projectList">
-                <li class="listItem">
-                    <a target="_blank" href="https://safe-citadel-26643.herokuapp.com/" 
-                    class="diaries">
-                        <button class="diariesButton">Diaries</button>
-                    </a>
-                    <a class="gitLink" href="https://github.com/Dlunoe/Project-Reviews">GitHub</a>
-                </li>
-                <li class="listItem">
-                    <a target="_blank" href="https://reviews-app-client.herokuapp.com/" 
-                    class="reviews">
-                        <button class="reviewsButton">Reviews</button>
-                    </a>
-                    <a class="gitLink" href="https://github.com/Dlunoe/ProjectGamereviews">GitHub</a>
-                </li>
-            </ul>
-        </div>
-    )
+class Projects extends Component{
+    componentDidMount=()=> {
+        fetch('https://safe-citadel-26643.herokuapp.com/', {mode: 'no-cors'})
+        .then(res=>console.log('woke up diaries'));
+
+        fetch('https://reviews-app-client.herokuapp.com/', {mode: 'no-cors'})
+        .then(res=>console.log('woke up reviews'));
+    }
+    render(){
+        return(
+            <div className="projects">
+                <ul class="projectList">
+                    <li class="listItem">
+                        <a target="_blank" href="https://safe-citadel-26643.herokuapp.com/" 
+                        class="diaries">
+                            <button class="diariesButton">Diaries</button>
+                        </a>
+                        <a class="gitLink" href="https://github.com/Dlunoe/Project-Reviews">GitHub</a>
+                    </li>
+                    <li class="listItem">
+                        <a target="_blank" href="https://reviews-app-client.herokuapp.com/" 
+                        class="reviews">
+                            <button class="reviewsButton">Reviews</button>
+                        </a>
+                        <a class="gitLink" href="https://github.com/Dlunoe/ProjectGamereviews">GitHub</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+    
 }
 
 export default Projects;
